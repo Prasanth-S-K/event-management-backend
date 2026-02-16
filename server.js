f0fdb5e75cf0f6fd302179b7e43323ade2b1e9ff
Prasanth-S-K <prasanthk12051999@gmail.com>
2026-02-16 19:50:28 +0530

    fixed cors issue in server.js

--- server.js
+++ server.js
@@ -16,12 +16,30 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://event-management-frontend-zeta-nine.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        console.log("Origin rejected:", origin);
+        return callback(new Error("CORS not allowed"), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 
@@ -30,14 +48,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-
 app.get("/api/protected", protect, (req, res) => {
   res.json({
     message: "Protected route accessed",
     user: req.user,
   });
 });
-
 app.use("/api/events", eventRoutes);
 app.use("/api/registrations", registrationRoutes);
 
